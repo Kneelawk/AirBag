a016de9942788f66d74eba96e827066c02ee1975
Kneelawk <kneelawk@gmail.com>
2023-12-29 05:50:06 -0800

    Fix AE2 recipes with KubeJS

---
+++ src/main/java/com/kneelawk/airbag/kubejs/AirBagKubeJSPlugin.java
@@ -0,0 +1,44 @@
+package com.kneelawk.airbag.kubejs;
+
+import appeng.core.definitions.AEItems;
+import appeng.recipes.entropy.EntropyRecipeSerializer;
+import appeng.recipes.game.FacadeRecipe;
+import appeng.recipes.handlers.ChargerRecipeSerializer;
+import appeng.recipes.handlers.InscriberRecipeSerializer;
+import appeng.recipes.mattercannon.MatterCannonAmmoSerializer;
+import appeng.recipes.transform.TransformRecipeSerializer;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.JsonRecipeSchemaType;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeNamespace;
+import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
+
+import org.quiltmc.loader.api.QuiltLoader;
+
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.util.Identifier;
+
+public class AirBagKubeJSPlugin extends KubeJSPlugin {
+    @Override
+    public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
+        if (QuiltLoader.isModLoaded("ae2")) {
+            RecipeNamespace ae2 = event.namespace("ae2");
+            ae2.put("charger",
+                new JsonRecipeSchemaType(ae2, new Identifier("ae2:charger"), ChargerRecipeSerializer.INSTANCE));
+            ae2.put("inscriber",
+                new JsonRecipeSchemaType(ae2, new Identifier("ae2:inscriber"), InscriberRecipeSerializer.INSTANCE));
+            ae2.put("transform",
+                new JsonRecipeSchemaType(ae2, new Identifier("ae2:transform"), TransformRecipeSerializer.INSTANCE));
+            ae2.put("entropy",
+                new JsonRecipeSchemaType(ae2, new Identifier("ae2:entropy"), EntropyRecipeSerializer.INSTANCE));
+            ae2.put("matter_cannon", new JsonRecipeSchemaType(ae2, new Identifier("ae2:matter_cannon"),
+                MatterCannonAmmoSerializer.INSTANCE));
+            ae2.put("facade", new JsonRecipeSchemaType(ae2, new Identifier("ae2:facade"), null) {
+                @Override
+                public RecipeSerializer<?> getSerializer() {
+                    // delay until after AE2 is loaded
+                    return FacadeRecipe.getSerializer(AEItems.FACADE.asItem());
+                }
+            });
+        }
+    }
+}
